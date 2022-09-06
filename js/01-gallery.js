a9ad0ad362460254cd4214b631c5c081070c77e0
Vitaliy-Danilchik <danilchikvvv@gmail.com>
2022-09-07 00:04:07 +0300

    go-js-hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML("afterbegin", createGalleryCards(galleryItems));
+
+
+function createGalleryCards(galleryItems) {
+	return galleryItems.map(({ preview, original, description }) => {
+		return `<div class="gallery__item">
+	<a class="gallery__link" href="${original}">
+	  <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+	</a>
+ </div>` }).join('');
+};
+
+gallery.addEventListener('click', onItemHandler);
+
+function onItemHandler(event) {
+	event.preventDefault();
+
+	if (!event.target.classList.contains("gallery__image")) {
+		return;
+	}
+
+	pictureInModal(event.target.dataset.source);
+}
+
+function pictureInModal(originalImageLink) {
+	const instance = basicLightbox.create(`
+    <img src="${originalImageLink}" width="800" height="600">
+`)
+
+	instance.show();
+	closeByKeybord(instance);
+}
+
+function closeByKeybord(instance) {
+	document.addEventListener("keydown", (e) => {
+		if (e.code == "Escape") {
+			instance.close()
+		}
+	}, { once: true });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML("afterbegin", createGalleryCards(galleryItems));
+
+
+function createGalleryCards(galleryItems) {
+	return galleryItems.map(({ preview, original, description }) => {
+		return `<a class="gallery__item" href="${original}"}><img class="gallery__image" src="${preview}" alt="${description}"/>
+	 </a>` }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
